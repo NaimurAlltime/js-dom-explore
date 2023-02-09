18dedd8fd397fa45252e2b644d228d255b413889
Naimur Rahman <naimur2231@gmail.com>
2023-02-09 23:57:50 +0600

    explore getElementById and getElementsByClassName

--- js/dom.js
+++ js/dom.js
@@ -10,3 +10,11 @@ const h3Collection = document.getElementsByTagName('h3');
 for(const h3 of h3Collection){
     console.log(h3.innerText);
 }
+
+document.getElementById('title').innerText = "EXPLORE DOM";
+
+const languageCollection = document.getElementsByClassName('programing_language');
+
+for(const language of languageCollection){
+    console.log(language.innerText);
+}
